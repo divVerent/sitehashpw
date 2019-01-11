df1e5a82abfe63226c38d4528184f00064f9f939
Rudolf Polzer <divVerent@xonotic.org>
2019-01-11 09:56:40 -0500

    Add an icon.

--- pack.sh
+++ pack.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
 set -ex
+
 sh LICENSE > LICENSE.html
+
+for size in 16 48 128; do
+	convert -size "$size"x"$size" icon.svg icon-"$size".png
+done
+
 sh <<EEOF > manifest.json
 cat <<EOF
 `cat manifest.in.json`
 EOF
 EEOF
+
 rm -f extension.zip
 zip -9r extension.zip \
 	manifest.json \
 	argon2-browser/dist/argon2-asm.min.js \
+	icon-*.png \
 	sitepw.js \
 	sitepw-extension* \
 	sitepw-frontend.css \
